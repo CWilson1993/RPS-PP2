6d59379bc0dcccef9fb87bbd06d33430bb8c45b8
CWilson1993 <cian.wilson10@gmail.com>
2022-05-02 12:02:08 +0000

    Add initial code to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const computerPickDisplay = document.getElementsByClassName('comp-pick');
+const userPickDisplay = document.getElementsByClassName('user-pick');
+const resultDisplay = document.getElementsByClassName('result');
+const possibleChoices = document.querySelectorAll('button');
+let userPick;
+let computerPick;
+let result;
