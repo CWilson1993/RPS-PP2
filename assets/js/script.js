79e3a0c740eb896a57a34765291252cddbea1a85
CWilson1993 <cian.wilson10@gmail.com>
2022-05-02 19:02:31 +0000

    Add function to generate result for all scenarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,51 @@ let result
 
 
 choices.forEach(choice => choice.addEventListener('click', (e) => {
-    userPick = e.target.id
+    userPick = e.target.value
     userPickDisplay.innerHTML = userPick
     generateComputerPick()
+    getResult()
 }))
 
 function generateComputerPick() {
     const randomPick = Math.floor(Math.random() * choices.length) + 1
 
-    if (randomPick === 1){
+    if (randomPick === 1) {
         computerPick = 'Rock'
     }
-    if (randomPick === 2){
+    if (randomPick === 2) {
         computerPick = 'Paper'
     }
-    if (randomPick === 3){
+    if (randomPick === 3) {
         computerPick = 'Scissors'
     }
     computerPickDisplay.innerHTML = computerPick
 
+}
+
+const getResult = () => {
+    switch (userPick + computerPick) {
+        case 'ScissorsPaper':
+            resultDisplay.innerHTML = userPick + " cuts " + computerPick + ", you win!" 
+            break
+        case 'RockScissors':
+            resultDisplay.innerHTML = userPick + " smashes " + computerPick + ", you win!" 
+            break
+        case 'PaperRock':
+            resultDisplay.innerHTML = userPick + " covers " + computerPick + ", you win!" 
+            break
+        case 'PaperScissors':
+            resultDisplay.innerHTML = "Oh no you lose as  " + computerPick + " covers " + userPick
+            break
+        case 'ScissorsRock':
+            resultDisplay.innerHTML = "Oh no you lose as  " + computerPick + " smashes " + userPick
+        case 'RockPaper':
+            resultDisplay.innerHTML = "Oh no you lost as " + computerPick + " covers " + userPick
+            break
+        case 'PaperPaper':
+        case 'ScissorsScissors':
+        case 'RockRock':
+            resultDisplay.innerHTML = "You both picked " + userPick + ", it's a draw!"
+            break
+    }
 }
