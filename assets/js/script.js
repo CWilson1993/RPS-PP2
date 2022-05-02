116c7ef8088c7df4310c4dcaebb2e333504ca9e5
CWilson1993 <cian.wilson10@gmail.com>
2022-05-02 18:24:12 +0000

    Add computer random choice function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,30 @@
-const computerPickDisplay = document.getElementsByClassName('comp-pick'); //gets the location in the HTML by the class name
-const userPickDisplay = document.getElementsByClassName('user-pick');
-const resultDisplay = document.getElementsByClassName('result');
-const choices = document.querySelectorAll('button');
-let userPick;
-let computerPick;
-let result;
-
-choices.forEach(choice => choice.addEventListener('click', (e) => { 
-    userPick = e.target.id;
-    userPickDisplay.innerHTML = userPick;
-    generateComputerPick();
-    getResult();
-}));
+const computerPickDisplay = document.getElementById('comp-pick') //gets the location in the HTML by the class name
+const userPickDisplay = document.getElementById('user-pick')
+const resultDisplay = document.getElementById('result')
+const choices = document.querySelectorAll('button')
+let userPick
+let computerPick
+let result
 
 
+choices.forEach(choice => choice.addEventListener('click', (e) => {
+    userPick = e.target.id
+    userPickDisplay.innerHTML = userPick
+    generateComputerPick()
+}))
 
+function generateComputerPick() {
+    const randomPick = Math.floor(Math.random() * choices.length) + 1
 
+    if (randomPick === 1){
+        computerPick = 'Rock'
+    }
+    if (randomPick === 2){
+        computerPick = 'Paper'
+    }
+    if (randomPick === 3){
+        computerPick = 'Scissors'
+    }
+    computerPickDisplay.innerHTML = computerPick
 
+}
